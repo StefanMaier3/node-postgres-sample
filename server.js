90fd54476d03b20730f2099d728dee9433db3b63
stefan.maier@platoyo.com <stefan.maier@platoyo.com>
2023-04-18 17:22:54 +0200

    no sequel

--- server.js
+++ server.js
@@ -2,38 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || "todos",
-  process.env.DB_USER || "todos",
-  process.env.DB_PASSWORD || "password",
-  {
-    host: process.env.DB_HOST || "localhost",
-    dialect: "postgres",
-    pool: {
-      max: 10,
-      min: 1,
-    },
-  }
-);
-const Todo = sequelize.define("todo", {
-  title: {
-    type: Sequelize.STRING,
-  },
-  completed: {
-    type: Sequelize.BOOLEAN,
-  },
-});
-
-sequelize.sync().then(() => {
-  Todo.count({ title: { [Sequelize.Op.Eq]: "Something" } }).then(
-    (itemCount) => {
-      if (itemCount === 0) {
-        Todo.create({ title: "Something", completed: false });
-      }
-    }
-  );
-});
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -43,14 +11,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/todos", (req, res) => {
-  Todo.findAll()
-    .then((data) => res.send(data))
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving tutorials.",
-      });
-    });
+  res.send("keine datenbank moruk")
 });
 
 // set port, listen for requests
